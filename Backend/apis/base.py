a9c61f87318ff70a492df24cc4d3633eb50e675a
GoWoong <popgun10@naver.com>
2022-11-17 22:34:30 +0900

    pre-coommit

--- Backend/apis/base.py
+++ Backend/apis/base.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-
 from apis.version1 import route_general_pages
 from apis.version1 import route_users
+from apis.version1 import route_jobs
 
 
 api_router = APIRouter()
@@ -9,3 +9,4 @@ api_router.include_router(
     route_general_pages.general_pages_router, prefix="", tags=["general_pages"]
 )
 api_router.include_router(route_users.router, prefix="/users", tags=["users"])
+api_router.include_router(route_jobs.router, prefix="/jobs", tags=["jobs"])
