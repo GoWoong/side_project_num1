8b900236d6ceb4b01e75d4b586675b51c7bf5967
GoWoong <popgun10@naver.com>
2022-11-17 22:21:48 +0900

    study pytest setting

--- Backend/apis/version1/route_general_pages.py
+++ Backend/apis/version1/route_general_pages.py
@@ -4,10 +4,10 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="templates")
-general_pates_router = APIRouter()
+general_pages_router = APIRouter()
 
 
-@general_pates_router.get("/")
+@general_pages_router.get("/")
 async def home(request: Request):
     return templates.TemplateResponse(
         "general_pages/homepage.html", {"request": request}
